390d1588117ffe207024899e9b49342f6bdf95b4
Thiago Cruz <thiago.cruz@virtus.ufcg.edu.br>
2023-08-31 03:53:09 -0300

    Correções no modelo do motor e modelo dinâmico

--- Dynamic_Model.py
+++ Dynamic_Model.py
@@ -1,4 +1,5 @@
-import sympy as sp
+import control as ctrl
+import numpy as np
 
 class Dynamic_Model:
     def __init__(self):
@@ -6,7 +7,7 @@ class Dynamic_Model:
         self.R  = self.D/2 # Raio das rodas
         self.mc = 13       # Massa do chassi
         self.mw = 1.5      # Massa da roda + motor
-        self.m = self.mc + 2*self.mw; # Massa total do robô
+        self.M = self.mc + 2*self.mw; # Massa total do robô
         self.L = 0.1655    # Largura do robô/2
         self.d = 44.51e-3  # Distância do centro de massa ao eixo das rodas
         self.Ic = 130.7e-3 # Momento de inércia do chassi
@@ -14,15 +15,47 @@ class Dynamic_Model:
         self.Im = 20e-3    # Momento de inércia da roda em torno do diâmetro
         self.I  = self.Ic + self.mc*self.d**2 + 2*self.mw*self.L**2 + 2*self.Im # Inércia total
 
-        # Variáveis simbólicas
-        self.omega_right, self.omega_left = sp.symbols('omega_right omega_left')
+        self.omega_0 = 0
+        self.vu_0 = 0
 
 
-    def step(self, torque_right, torque_left):
-        omega_right = (self.R * torque_right) / self.I
-        omega_left = (self.R * torque_left) / self.I
+    def step(self, tau_r, tau_l):
+        s = ctrl.TransferFunction.s
+        delta_t = 0.05000000074505806
+        s_value = 2*np.pi*1/delta_t
 
-        return omega_right, omega_left
+
+        up_0 = tau_r*1/self.R
+        down_0 = tau_l*1/self.R
+
+        up_1 = up_0 + down_0
+        down_1 = up_0 - down_0
+
+        up_2 = up_1/self.M
+        down_2 = down_1*self.L
+
+        up_3 = up_2+self.d*self.omega_0
+        down_3 = down_2 - self.vu_0*self.M*self.d
+
+        vu = up_3/s
+        omega = down_3/s
+
+        aux = omega*self.L
+
+        down_4 = vu - aux
+        up_4 = aux + vu
+
+        phi_R = up_4 * 1/self.R
+        phi_L = down_4 * 1/self.R
+
+        self.omega_0 = np.abs(ctrl.evalfr(omega, 1j*s_value))
+        self.vu_0 = np.abs(ctrl.evalfr(vu, 1j*s_value))
+
+        phi_R_value = np.abs(ctrl.evalfr(phi_R, 1j*s_value))
+        phi_L_value = np.abs(ctrl.evalfr(phi_L, 1j*s_value))
+
+        return phi_R_value, phi_L_value 
+
 
 # Exemplo de uso:
 if __name__ == "__main__":
--- MotorModel.py
+++ MotorModel.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 
 class MotorModel:
     def __init__(self):
@@ -8,31 +9,67 @@ class MotorModel:
         self.Ke = 23e-3   # Constante de força eletromotriz (Volts por unidade de velocidade angular)
         self.Kt = 29e-3   # Constante de torque (Nm por Ampere)
 
-    def step(self, voltage_right, voltage_left, angular_velocity_right, angular_velocity_left):
-        # Calcule a corrente nas rodas direita e esquerda com base nas tensões aplicadas
-        current_right = (voltage_right - self.Ke * angular_velocity_right) / (self.R + self.L * angular_velocity_right)
-        current_left = (voltage_left - self.Ke * angular_velocity_left) / (self.R + self.L * angular_velocity_left)
+        # Inicialize as condições iniciais para as correntes e torques
+        self.I0_1 = 0.0
+        self.I0_2 = 0.0
+        self.torque0_1 = 0.0
+        self.torque0_2 = 0.0
 
-        # Calcule o torque nas rodas direita e esquerda com base na corrente
-        torque_right = self.Kt * current_right
-        torque_left = self.Kt * current_left
+    def motor_equations(self, t, state, voltage, omega):
+        I = state[0]
+        E = self.Ke * omega  # Cálculo da constante de eletromotriz (volts)
+        dI_dt = (voltage - I * self.R - E) / self.L
+        torque_m = self.Kt * I
+        torque = self.N*torque_m
+        return [dI_dt, torque]
 
-        return torque_right, torque_left
-    
-# Exemplo de uso:
+    def step(self, voltage1, voltage2, omega1, omega2, t):
+        # Integre as equações diferenciais para calcular as correntes e os torques no próximo ponto de tempo
+        solution_1 = solve_ivp(
+            fun=lambda t, y: self.motor_equations(t, y, voltage1, omega1),
+            t_span=(t, t + 0.01),  # Próximo ponto de tempo
+            y0=[self.I0_1, self.torque0_1],
+            t_eval=[t + 0.01],
+            method='RK45' #Runge-Kutta
+        )
+
+        solution_2 = solve_ivp(
+            fun=lambda t, y: self.motor_equations(t, y, voltage2, omega2),
+            t_span=(t, t + 0.01),  # Próximo ponto de tempo
+            y0=[self.I0_2, self.torque0_2],
+            t_eval=[t + 0.01],
+            method='RK45' #Runge-Kutta
+        )
+
+        I1 = solution_1.y[0]
+        I2 = solution_2.y[0]
+
+        torque_1 = solution_1.y[1]
+        torque_2 = solution_2.y[1]
+
+        self.I0_1 = I1[0]
+        self.I0_2 = I2[0]
+        self.torque_01 = torque_1[0]
+        self.torque_02 = torque_2[0]
+
+        return torque_1[0], torque_2[0]
+
 if __name__ == "__main__":
+    # Crie uma instância do motor CC
+    motor = MotorModel()
 
-    # Crie um modelo de motor com os parâmetros especificados
-    motor_model = MotorModel()
+    # Tensões e velocidades angulares de entrada
+    voltage1 = 12.0  # Tensão para o primeiro motor (volts)
+    voltage2 = 8.0   # Tensão para o segundo motor (volts)
+    omega1 = 100.0   # Velocidade angular para o primeiro motor (rad/s)
+    omega2 = 50.0    # Velocidade angular para o segundo motor (rad/s)
 
-    # Aplique tensões às rodas direita e esquerda e obtenha as velocidades angulares (ajuste conforme necessário)
-    voltage_right = 12.0  # Volts
-    voltage_left = 10.0  # Volts
-    angular_velocity_right = 0.0  # Unidades de velocidade angular (ajuste conforme necessário)
-    angular_velocity_left = 0.0  # Unidades de velocidade angular (ajuste conforme necessário)
+    # Tempo inicial
+    t = 0.0
 
-    # Calcule o torque nas rodas direita e esquerda
-    torque_right, torque_left = motor_model.step(voltage_right, voltage_left, angular_velocity_right, angular_velocity_left)
+    # Calcule as correntes e os torques no próximo ponto de tempo
+    torque1, torque2 = motor.step(voltage1, voltage2, omega1, omega2, t)
 
-    print(f"Torque na Roda Direita: {torque_right} Nm")
-    print(f"Torque na Roda Esquerda: {torque_left} Nm")
+    # Exiba os resultados
+    print("Torque no primeiro motor no próximo ponto de tempo:", torque1)
+    print("Torque no segundo motor no próximo ponto de tempo:", torque2)
